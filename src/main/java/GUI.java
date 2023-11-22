f679fd569857105f450a5d8fbafa50c6a64f5538
AndreyKobetskiy <ronorothog2003@gmail.com>
2023-11-22 20:42:30 +0300

    Date visibility bug fix

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -138,6 +138,8 @@ public class GUI {
                 data.remove(lastNote);
                 textArea.setText("");
                 nameField.setText("");
+                creationDate.setText("");
+                lastSeenDate.setText("");
                 lastNote = null;
             }
             mainFrame.revalidate();
